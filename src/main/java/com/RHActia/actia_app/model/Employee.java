20ad8c23128502dcf536224dfe509c9c00f476ca
montassargassara <montassar.gassara01@gmail.com>
2024-03-01 15:05:09 +0100

    eyafekibackend1

--- src/main/java/com/RHActia/actia_app/model/Employee.java
+++ src/main/java/com/RHActia/actia_app/model/Employee.java
@@ -52,5 +52,43 @@ public class Employee {
         this.employeeImages = employeeImages;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
